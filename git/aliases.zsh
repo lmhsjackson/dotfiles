4a027f7c910c6c37ab808b8601983275fe475e92
Eloy Espinaco <eloyesp@gmail.com>
2013-05-06 11:05:20 -0300

    Add alias to delete merged branches gbd.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -24,4 +24,4 @@ alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
-
+alias gbd="git branch --merged | grep -v master | grep -v '*' | xargs git branch -d"
