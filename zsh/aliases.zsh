5f5d90973a1ad4c9bcc67af0905560bc14873c45
Jose Piccioni <lmhsjackson@gmail.com>
2015-06-08 17:38:35 -0300

    add my personal aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,10 @@ alias reload!='. ~/.zshrc'
 alias ls="ls --color=auto"
 alias ll="ls -l"
 alias la="ll --almost-all"
+
+# My personal alias
+alias rt='rake test'
+alias be='bundle exec'
+alias si='sudo apt-get install'
+alias sr='sudo apt-get remove --purge'
+alias esc='xmodmap ~/.xmodmap'
